e5b06bcf599cedfa610c89d2236eb82a86ef4cdb
DaniilKosmachev <fame37@yandex.ru>
2023-07-23 18:00:12 +0300

    Домашнее задание 8 спринта, версия 1

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button_settings = findViewById<Button>(R.id.setting_window)
+        val button_search = findViewById<Button>(R.id.search_window)
+        val button_library = findViewById<Button>(R.id.library_window)
+        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(button: View?) {
+                val openSettingsWindow = Intent(this@MainActivity, SettingsActivity::class.java)
+                startActivity(openSettingsWindow)
+            }
+        }
+        button_search.setOnClickListener {
+            val openSearchWindow = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(openSearchWindow)
+        }
+        button_settings.setOnClickListener(buttonClickListener)
+        button_library.setOnClickListener(this@MainActivity)
+    }
+
+    override fun onClick(button: View?) {
+        when (button?.id) {
+            R.id.library_window -> {
+                val openLibraryWindow = Intent(this@MainActivity, LibraryActivity::class.java)
+                startActivity(openLibraryWindow)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,22 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.app.ShareCompat.IntentReader
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        val buttonBackFromMainActivity = findViewById<ImageButton>(R.id.backToMainActivityViewButton)
+        buttonBackFromMainActivity.setOnClickListener {
+            val backToMainActivity = Intent(this, MainActivity::class.java)
+            startActivity(backToMainActivity)
+        }
+    }
+
+}
