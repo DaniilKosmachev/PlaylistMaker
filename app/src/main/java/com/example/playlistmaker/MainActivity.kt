d2b49f23da491edbd2320b1c4e01161d7d3e2fc3
DaniilKosmachev <fame37@yandex.ru>
2023-07-24 20:30:07 +0300

    Домашнее задание 8 спринта, версия 3 1) Поменял названия переменных в MainActivity.kt на camelCase 2) Везде посмотрел, поменял цвета вместо конкретных # на @colors 3) Убрал вложенность лишнюю на activity_main.xml (удалил FrameLayout) 4) В activity_main.xml убрал конкретную высоту, поменяв на 0dp в dimens.xml, layout_weight выставил всем button по 1 5) Убрав clickable у LinerLayout в activity_settings.xml при запуске на эмуляторе пропал градиент (от касания расплывается серый фон по layout), пока оставил так, ибо непонятно, проходит click или нет - если от этого не зависит, то плз, напишите)))) 6) В activity_settings.xml layout_width поменял на dimens из пункта 4 (т.е. на 0dp) 7) В dimens.xml поменял кратность padding на 16dp, соответственно, оставил один dimen с name settings_padding_vertical

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,32 +1,30 @@
 package com.example.playlistmaker
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button_settings = findViewById<Button>(R.id.setting_window)
-        val button_search = findViewById<Button>(R.id.search_window)
-        val button_library = findViewById<Button>(R.id.library_window)
+        val buttonSettings = findViewById<Button>(R.id.setting_window)
+        val buttonSearch = findViewById<Button>(R.id.search_window)
+        val buttonLibrary = findViewById<Button>(R.id.library_window)
         val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
             override fun onClick(button: View?) {
                 val openSettingsWindow = Intent(this@MainActivity, SettingsActivity::class.java)
                 startActivity(openSettingsWindow)
             }
         }
-        button_search.setOnClickListener {
+        buttonSearch.setOnClickListener {
             val openSearchWindow = Intent(this@MainActivity, SearchActivity::class.java)
             startActivity(openSearchWindow)
         }
-        button_settings.setOnClickListener(buttonClickListener)
-        button_library.setOnClickListener(this@MainActivity)
+        buttonSettings.setOnClickListener(buttonClickListener)
+        buttonLibrary.setOnClickListener(this@MainActivity)
     }
 
     override fun onClick(button: View?) {
