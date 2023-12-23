3e4a4d3dcf213134188d0a3dcc8ab87fb9c8aff0
DaniilKosmachev <fame37@yandex.ru>
2023-12-23 19:48:51 +0300

    ДЗ 18, версия 2

--- app/src/main/java/com/example/playlistmaker/ui/library/fragments/LibActivityFavoriteTracksFragment.kt
+++ app/src/main/java/com/example/playlistmaker/ui/library/fragments/LibActivityFavoriteTracksFragment.kt
@@ -6,11 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.playlistmaker.databinding.LibraryActivityFragmentFavoriteTracksBinding
+import com.example.playlistmaker.ui.library.view_model.FavoriteTrackFragmentViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LibActivityFavoriteTracksFragment: Fragment() {
 
     private var _binding: LibraryActivityFragmentFavoriteTracksBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel by viewModel<FavoriteTrackFragmentViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +23,15 @@ class LibActivityFavoriteTracksFragment: Fragment() {
     ): View? {
         _binding = LibraryActivityFragmentFavoriteTracksBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     companion object {
--- app/src/main/java/com/example/playlistmaker/ui/library/fragments/LibActivityPlaylistsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/ui/library/fragments/LibActivityPlaylistsFragment.kt
@@ -6,11 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.playlistmaker.databinding.LibraryActivityFragmentPlaylistsBinding
+import com.example.playlistmaker.ui.library.view_model.PlaylistsFragmentViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LibActivityPlaylistsFragment: Fragment() {
 
     private var _binding: LibraryActivityFragmentPlaylistsBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel by viewModel<PlaylistsFragmentViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +23,15 @@ class LibActivityPlaylistsFragment: Fragment() {
     ): View? {
         _binding = LibraryActivityFragmentPlaylistsBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     companion object {
